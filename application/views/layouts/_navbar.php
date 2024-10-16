cb28cbc6805d67366150a4c8647a651a37a7a097
unknown <cimisorang456@gmail.com>
2024-10-16 15:00:58 +0700

    tiga

--- application/views/layouts/_navbar.php
+++ application/views/layouts/_navbar.php
@@ -26,23 +26,23 @@
                     <a href="/cart.html" class="nav-link"><i class="fa-solid fa-cart-shopping"></i> cart (0)
                         </i></a>
                 </li>
-                <?php if (! $this->session->userdata('is_login')); ?>
-                <li class="nav-item">
-                    <a href="<?= base_url('/login') ?>" class="nav-link">login</a>
-                </li>
-                <li class="nav-item">
-                    <a href="<?= base_url('/register') ?>" class="nav-link">register</a>
-                </li>
-                <?php else ; ?>
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" id="dropdown-2" data-toggle="dropdown"
-                        aria-haspopup="true" aria aria-expanded="false"><?= $this->session->userdata('name') ?></a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown-2">
-                        <a href="/profile.html" class="dropdown-item">Profile</a>
-                        <a href="/orders.html" class="dropdown-item">Orders</a>
-                        <a href="<?= base_url('/logout') ?>" class="dropdown-item">Logout</a>
-                    </div>
-                </li>
+                <?php if (! $this->session->userdata('is_login')): ?>
+                    <li class="nav-item">
+                        <a href="<?= base_url('/login') ?>" class="nav-link">login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?= base_url('/register') ?>" class="nav-link">register</a>
+                    </li>
+                <?php else : ?>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" id="dropdown-2" data-toggle="dropdown"
+                            aria-haspopup="true" aria aria-expanded="false"><?= $this->session->userdata('name') ?></a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown-2">
+                            <a href="/profile.html" class="dropdown-item">Profile</a>
+                            <a href="/orders.html" class="dropdown-item">Orders</a>
+                            <a href="<?= base_url('/logout') ?>" class="dropdown-item">Logout</a>
+                        </div>
+                    </li>
                 <?php endif ?>
             </ul>
         </div>
